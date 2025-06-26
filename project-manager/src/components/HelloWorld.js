79b7fe8d4cd0d7ff5923afc6735af6cfbb44052a
Icaro Zelioli <i.augustomz@gmail.com>
2025-06-26 00:12:12 -0300

    ✨feature: adds the studies of components

---
+++ project-manager/src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import Phrase from "./Phrase"
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>This is my first component.</h1>
+            <Phrase />
+            <Phrase />
+        </div>
+    )
+}
+
+export default HelloWorld
