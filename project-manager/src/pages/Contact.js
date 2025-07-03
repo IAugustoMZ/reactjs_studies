77d5ab962b7d1978a4c07e408d7c9b12171badfd
Icaro Zelioli <i.augustomz@gmail.com>
2025-07-03 00:34:05 -0300

    ✨feature: implements react router dom

---
+++ project-manager/src/pages/Contact.js
@@ -0,0 +1,13 @@
+function Contact() {
+  return (
+    <div>
+        <h1>Contact Us</h1>
+        <p>If you have any questions, feel free to reach out!</p>
+        <p>Email: </p>
+        <p>Phone: </p>
+        <p>Address: </p>
+    </div>
+    );
+}
+
+export default Contact
