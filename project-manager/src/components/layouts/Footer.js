5ad98962f0b1b9a0d783a13df4acbdd83c179878
Icaro Zelioli <i.augustomz@gmail.com>
2025-07-03 00:47:01 -0300

    ✨feature: implements the react icons

--- project-manager/src/components/layouts/Footer.js
+++ project-manager/src/components/layouts/Footer.js
@@ -1,7 +1,15 @@
+import styles from './Footer.module.css';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
 function Footer() {
   return (
     <footer>
-      <p>Footer content goes here</p>
+        <ul className={styles.social_list}>
+            <li><FaFacebook/></li>
+            <li><FaInstagram/></li>
+            <li><FaLinkedin/></li>
+        </ul>
+        <p>Footer content goes here</p>
     </footer>
   );
 }
